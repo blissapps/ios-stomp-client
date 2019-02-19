c14c778fa9f5330985abe54d5e7efb370df10661
Tiago Janela <tjanela@blissapplications.com>
2019-02-19 09:39:39 +0000

    Added needed infrastructure for carthage support

---
+++ EIDAKStompClient/EIDAKStompClient/EIDAKStompClient.h
@@ -0,0 +1,19 @@
+//
+//  EIDAKStompClient.h
+//  EIDAKStompClient
+//
+//  Created by Tiago Janela on 14/02/2019.
+//  Copyright © 2019 Bliss Applications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EIDAKStompClient.
+FOUNDATION_EXPORT double EIDAKStompClientVersionNumber;
+
+//! Project version string for EIDAKStompClient.
+FOUNDATION_EXPORT const unsigned char EIDAKStompClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EIDAKStompClient/PublicHeader.h>
+
+
